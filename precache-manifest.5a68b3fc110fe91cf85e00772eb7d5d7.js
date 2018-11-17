ca61d0b3a83ff559b321f2e25b5e70cca5ec9e2a
Ivo <ivo.paunov@gmail.com>
2018-11-17 10:41:34 +0200

    Updates

---
+++ precache-manifest.5a68b3fc110fe91cf85e00772eb7d5d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/subaruparts/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d439cdd27423722bb75a",
+    "url": "/subaruparts/static/js/runtime~main.d439cdd2.js"
+  },
+  {
+    "revision": "86959504302b88d673d6",
+    "url": "/subaruparts/static/js/main.86959504.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/subaruparts/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "86959504302b88d673d6",
+    "url": "/subaruparts/static/css/main.c6a634b2.chunk.css"
+  },
+  {
+    "revision": "929de9633c091b676ee5ab1e2268e4f6",
+    "url": "/subaruparts/index.html"
+  }
+];
